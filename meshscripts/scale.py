0f71b133a6e82b97bba9d28ef45ab2fa72862349
Felix-012 <f.nuetzel@gmx.de>
2023-11-02 13:10:17 +0100

    fixed the scaling script, scaled and renamed all objects

--- meshscripts/scale.py
+++ meshscripts/scale.py
@@ -1,30 +1,32 @@
-import pymeshlab as ml
+
 import os
+import trimesh
+
 
 
 def scale_to_unit_sphere(input_path, output_path):
-    ms = ml.MeshSet()
-    ms.load_new_mesh(input_path)
+    mesh = trimesh.load_mesh(input_path)
 
-    # Compute the transformation matrix for normalization to unit sphere
-    ms.compute_matrix_from_scaling_or_normalization(unitflag=True)
+    mesh.apply_transform(trimesh.transformations.scale_matrix(1.0 / mesh.scale))
 
-    # Save the transformed mesh
-    ms.save_current_mesh(output_path, save_textures=False, texture_quality=-1)
+    mesh.export(output_path)
 
 
-def main(folder_path):
+
+
+def main(folder_path, scaled_folder_path):
     for filename in os.listdir(folder_path):
         if filename.endswith('.obj'):
+            print("Processing ", filename)
             input_path = os.path.join(folder_path, filename)
-            output_path = os.path.join(folder_path, 'scaled_' + filename)
+            output_path = os.path.join(scaled_folder_path, filename)
             scale_to_unit_sphere(input_path, output_path)
-            mtl_file = output_path.replace('.obj', '.obj.mtl')
-            if os.path.exists(mtl_file):
-                os.remove(mtl_file)
-            print(f"Processed and saved scaled mesh to {output_path}")
-
+    for filename in os.listdir(scaled_folder_path):
+        if not filename.endswith('.obj'):
+            os.remove(os.path.join(scaled_folder_path, filename))
+            print("Removing ", filename)
 
 if __name__ == "__main__":
-    folder = '/home/felix/uni/WS23/CompVisP/'  # Replace with your folder path
-    main(folder)
+    folder = '/home/felix/PycharmProjects/comp_visual_perception_ws_23_24/files/Objects'
+    scaled_folder = '/home/felix/PycharmProjects/comp_visual_perception_ws_23_24/files/Scaled_Objects'
+    main(folder, scaled_folder)
