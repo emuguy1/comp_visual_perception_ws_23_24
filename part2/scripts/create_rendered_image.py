f147b6a4fcc5a23ce2a6817d9c7f5109edd12277
Emanuel Erben <emanuel.erben@gmail.com>
2023-11-29 17:41:21 +0100

    Added part from Felix and added first examples

--- part2/scripts/create_rendered_image.py
+++ part2/scripts/create_rendered_image.py
@@ -27,40 +27,47 @@ scene.render.engine = 'CYCLES'  # Use the Cycles rendering engine
 scene.render.resolution_x = 512  # Set resolution width
 scene.render.resolution_y = 512  # Set resolution height
 
-# Set background to black
-world = bpy.data.worlds['World']
-world.use_nodes = True
-bg_node = world.node_tree.nodes.new(type='ShaderNodeBackground')
-bg_node.inputs['Color'].default_value = (0, 0, 0, 1)
-
-
 # Create and position the camera
 camera_data = bpy.data.cameras.new(name='Camera')
 camera_object = bpy.data.objects.new('Camera', camera_data)
 scene.collection.objects.link(camera_object)
 scene.camera = camera_object
 
-camera_data.lens = 10
+camera_data.lens = 90
 
-# Calculate the bounding box center of the object
-bbox_center = sum((mathutils.Vector(b) for b in imported_obj.bound_box), mathutils.Vector()) / 8
+def get_object_center(obj):
+    local_bbox_center = sum((mathutils.Vector(b) for b in obj.bound_box), mathutils.Vector()) / 8
+    return obj.matrix_world @ local_bbox_center
 
-# Function to calculate camera distance to fill a desired percentage of the frame
-def calculate_camera_distance(obj, camera, desired_fill):
-    # Get the dimensions of the object
+
+# Function to calculate the visible size of the object in the camera's direction
+def visible_size(obj, h_angle, v_angle):
     dimensions = obj.dimensions
+    # Adjustments to calculate size more accurately
+    size_x = dimensions.x * abs(math.cos(h_angle))
+    size_y = dimensions.y * abs(math.sin(h_angle))
+    size_z = dimensions.z * abs(math.sin(v_angle))
 
-    # Calculate the diagonal length of the bounding box
-    diagonal = math.sqrt(sum([dim ** 2 for dim in dimensions]))
+    # Combine components to get the visible size
+    return math.sqrt(size_x ** 2 + size_y ** 2 + size_z ** 2)
 
-    # Assuming the camera FoV is horizontal
-    camera_fov = camera.data.angle_x
+
+# Function to calculate camera distance
+def calculate_camera_distance(obj, camera, desired_fill, h_angle, v_angle):
+    # Visible size in the camera's direction
+    visible_obj_size = visible_size(obj, h_angle, v_angle)
+
+    # Camera's field of view calculation based on focal length
+    sensor_width = camera.data.sensor_width
+    focal_length = camera.data.lens
+    camera_fov = 2 * math.atan(sensor_width / (2 * focal_length))
 
     # Calculate the required distance
-    return diagonal / (2 * math.tan(camera_fov / 2)) / desired_fill
+    return visible_obj_size / (2 * math.tan(camera_fov / 2)) / desired_fill
 
 
 desired_fill = 1  # e.g., 80% fill
+
 
 # Function to orient the camera towards a target
 def look_at(obj, target):
@@ -78,18 +85,20 @@ for image_index in range(num_images):
     v_angle = math.radians(random.uniform(0, 45))
 
     # Update camera distance
-    distance = calculate_camera_distance(imported_obj, camera_object, desired_fill)
+    distance = calculate_camera_distance(imported_obj, camera_object, desired_fill, h_angle, v_angle)
+
+    # Calculate the actual center of the object
+    obj_center = get_object_center(imported_obj)
 
     # Calculate camera position
     camera_x = math.sin(h_angle) * distance * math.cos(v_angle)
     camera_y = math.cos(h_angle) * distance * math.cos(v_angle)
     camera_z = distance * math.sin(v_angle)
 
-    camera_object.location = bbox_center + mathutils.Vector((camera_x, -camera_y, camera_z))
+    camera_object.location = obj_center + mathutils.Vector((camera_x, -camera_y, camera_z))
 
     # Point the camera to the object
-    look_at(camera_object, bbox_center)
-
+    look_at(camera_object, obj_center)
 
     # Update the export path for each image
     scene.render.filepath = f"{export_path[:-4]}_{image_index}.png"
