68233e55e0bdbc3caa6737bc9e78947a9a8a9223
Emanuel Erben <emanuel.erben@gmail.com>
2023-12-07 15:33:24 +0100

    Rendered first batch

--- part2/scripts/create_rendered_image.py
+++ part2/scripts/create_rendered_image.py
@@ -4,22 +4,18 @@ import mathutils
 import math
 import random
 
-#Felix
-#folder_path = '/home/felix/PycharmProjects/'
-#python_path = TODO
-#Emanuel
+# Felix
+# folder_path = '/home/felix/PycharmProjects/'
+# python_path = TODO
+# Emanuel
 folder_path = 'D:/programmierte_programme/githubworkspace/'
 python_path = 'C:/Users/emanu/AppData/Local/Programs/Python/Python37/python.exe'
 
 # Path to your OBJ file
-filenum = str(4)
-obj_path = folder_path+'comp_visual_perception_ws_23_24/part2/files/distance_mesh/'+filenum+'.obj'
-export_path_normal = folder_path+'comp_visual_perception_ws_23_24/part2/files/images_cloth/'+filenum+'.png'
-export_path_distance = folder_path+'comp_visual_perception_ws_23_24/part2/files/images_distance_cloth/'+filenum+'.png'
-obj_path_sample = folder_path+'comp_visual_perception_ws_23_24/part2/files/sample_mesh/'+filenum+'.obj'
-export_path_sample = folder_path+'comp_visual_perception_ws_23_24/part2/files/sample_mesh_images/'+filenum+'/.png'
+filec = 1
 is_distance_mesh = False
-is_sample_run = True
+is_sample_run = False
+
 
 def render_image():
     # Clear existing objects
@@ -40,7 +36,7 @@ def render_image():
     bpy.context.view_layer.objects.active = imported_obj
     bpy.ops.object.shade_smooth()
 
-    #Enable vertex color if is distance mesh
+    # Enable vertex color if is distance mesh
     if is_distance_mesh:
         # create new material and add vertex color as a color attribute
         mat = bpy.data.materials.new(name="VertexMat")
@@ -48,13 +44,11 @@ def render_image():
         mat.node_tree.nodes["Principled BSDF"].inputs[3].default_value = (1, 1, 1, 1)
         mat.node_tree.nodes.new(type='ShaderNodeVertexColor')
         mat.node_tree.nodes["Color Attribute"].layer_name = "Color"
-        mat.node_tree.links.new(mat.node_tree.nodes["Color Attribute"].outputs[0], mat.node_tree.nodes["Principled BSDF"].inputs[0])
+        mat.node_tree.links.new(mat.node_tree.nodes["Color Attribute"].outputs[0],
+                                mat.node_tree.nodes["Principled BSDF"].inputs[0])
         imported_obj.data.materials.append(mat)
         bpy.context.object.active_material.shadow_method = 'NONE'
         bpy.context.object.active_material.diffuse_color = (1, 1, 1, 1)
-
-
-
 
     # Set up rendering parameters
     scene = bpy.context.scene
@@ -63,15 +57,11 @@ def render_image():
     scene.render.resolution_y = 512  # Set resolution height
     scene.cycles.device = 'GPU'  # Use GPU rendering
 
-
-
     bpy.ops.object.light_add(type='SUN', radius=2, align='WORLD', location=(150, 150, 0))
     if is_distance_mesh:
         bpy.context.object.data.cycles.cast_shadow = False
     else:
         bpy.context.object.cycles.shadow_terminator_offset = 0.1
-
-
 
     # Create and position the camera
     camera_data = bpy.data.cameras.new(name='Camera')
@@ -85,7 +75,6 @@ def render_image():
         local_bbox_center = sum((mathutils.Vector(b) for b in obj.bound_box), mathutils.Vector()) / 8
         return obj.matrix_world @ local_bbox_center
 
-
     # Function to calculate the visible size of the object in the camera's direction
     def visible_size(obj, h_angle, v_angle):
         dimensions = obj.dimensions
@@ -96,7 +85,6 @@ def render_image():
 
         # Combine components to get the visible size
         return math.sqrt(size_x ** 2 + size_y ** 2 + size_z ** 2)
-
 
     # Function to calculate camera distance
     def calculate_camera_distance(obj, camera, desired_fill, h_angle, v_angle):
@@ -111,9 +99,7 @@ def render_image():
         # Calculate the required distance
         return visible_obj_size / (2 * math.tan(camera_fov / 2)) / desired_fill
 
-
     desired_fill = 1  # e.g., 80% fill
-
 
     # Function to orient the camera towards a target
     def look_at(obj, target):
@@ -132,8 +118,8 @@ def render_image():
         # Random horizontal and vertical angles
         h_angles = [216, 0, 324, 72, 252, 108, 144, 180, 36, 288]
         v_angles = [19, 28, 7, 41, 34]
-        h_angle = math.radians(h_angles.pop(math.floor(image_index / 5))+image_index)
-        v_angle = math.radians(v_angles.pop(image_index % 5)+ image_index % 4)
+        h_angle = math.radians(h_angles.pop(math.floor(image_index / 5)) + image_index)
+        v_angle = math.radians(v_angles.pop(image_index % 5) + image_index % 4)
         if is_sample_run:
             v_angle = math.radians(45)
             h_angles = [216, 0, 324, 72, 252, 108, 144, 180, 36, 288]
@@ -166,9 +152,18 @@ def render_image():
         bpy.ops.render.render(write_still=True)
 
 
-render_image()
-if not is_sample_run:
-    is_distance_mesh = True
+for i in range(1, 10):
+    filenum = str(filec)
+    obj_path = folder_path + 'comp_visual_perception_ws_23_24/part2/files/distance_mesh/' + filenum + '.obj'
+    export_path_normal = folder_path + 'comp_visual_perception_ws_23_24/part2/files/images_cloth/' + filenum + '.png'
+    export_path_distance = folder_path + 'comp_visual_perception_ws_23_24/part2/files/images_distance_cloth/' + filenum + '.png'
+    obj_path_sample = folder_path + 'comp_visual_perception_ws_23_24/part2/files/sample_mesh/' + filenum + '.obj'
+    export_path_sample = folder_path + 'comp_visual_perception_ws_23_24/part2/files/sample_mesh_images/' + filenum + '/.png'
+    is_distance_mesh = False
     render_image()
+    if not is_sample_run:
+        is_distance_mesh = True
+        render_image()
+    filec = filec + 1
 
-os.system(python_path+" "+folder_path+"comp_visual_perception_ws_23_24/part2/scripts/make_background_black.py")
+os.system(python_path + " " + folder_path + "comp_visual_perception_ws_23_24/part2/scripts/make_background_black.py")
